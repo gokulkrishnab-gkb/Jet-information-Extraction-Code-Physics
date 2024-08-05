6fadd603a36b6946a8363a598ce56783da7fd189
Gokul Krishna B <167256613+gokulkrishnab-gkb@users.noreply.github.com>
2024-08-05 13:04:16 +0530

    Update jet_extract.C
    This will add few more information and will save the jet information in a csv file named jet_extract.csv

--- jet_extract.C
+++ jet_extract.C
@@ -4,11 +4,13 @@ R__LOAD_LIBRARY(libDelphes)
 #include "external/ExRootAnalysis/ExRootTreeReader.h"
 #endif
 
+#include <fstream>
+
 //------------------------------------------------------------------------------
 
 void jet_extract(const char *inputFile)
 {
-  gSystem->Load("/home/kousik/Tools/Delphes-3.5.0/libDelphes.so"); // modified as per the system directories.
+  gSystem->Load("/home/kousik/Tools/Delphes-3.5.0/libDelphes.so");
 
   TChain chain("Delphes");
   chain.Add(inputFile);
@@ -18,6 +20,9 @@ void jet_extract(const char *inputFile)
 
   TClonesArray *branchJet = treeReader->UseBranch("Jet");
 
+  std::ofstream outFile("jet_extract.csv");
+  outFile << "Event_Number,Jet_No,Px,Py,Pz,Energy,Mass,Eta,Phi\n";
+
   for (Int_t entry = 0; entry < numberOfEntries; ++entry)
   {
     treeReader->ReadEntry(entry);
@@ -25,7 +30,6 @@ void jet_extract(const char *inputFile)
     
     if (numberOfJets > 0)
     {
-      cout << "Event No: " << entry + 1 << endl;
       for (Int_t jetIndex = 0; jetIndex < numberOfJets; ++jetIndex)
       {
         Jet *jet = (Jet*) branchJet->At(jetIndex);
@@ -33,13 +37,32 @@ void jet_extract(const char *inputFile)
         double py = jet->PT * sin(jet->Phi);
         double pz = jet->PT * sinh(jet->Eta);
         double E = sqrt(px * px + py * py + pz * pz + jet->Mass * jet->Mass);
-        cout << "Jet No: " << jetIndex + 1 
+        
+        // Print to console
+        cout << "Event No: " << entry + 1 
+             << " , Jet No: " << jetIndex + 1 
              << " , Px = " << px 
              << " , Py = " << py 
              << " , Pz = " << pz 
              << " , E = " << E 
+             << " , Mass = " << jet->Mass 
+             << " , Eta = " << jet->Eta 
+             << " , Phi = " << jet->Phi 
              << endl;
+        
+        // Write to CSV file
+        outFile << entry + 1 << "," 
+                << jetIndex + 1 << "," 
+                << px << "," 
+                << py << "," 
+                << pz << "," 
+                << E << "," 
+                << jet->Mass << "," 
+                << jet->Eta << "," 
+                << jet->Phi << "\n";
       }
     }
   }
+
+  outFile.close();
 }
